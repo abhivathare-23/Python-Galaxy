b5172c52c1da81deb5f84d369b190734db79d726
Abhishek Vathare <107718018+abhivathare-23@users.noreply.github.com>
2025-06-17 20:39:52 +0530

    Add files via upload

---
+++ Modify_string.py
@@ -0,0 +1,17 @@
+# Modify String
+
+a = "python"
+
+print(a.upper())       # Converts all characters to uppercase
+
+print(a.lower())        # Converts all characters to lowercase
+
+b = " Hello Java"
+print(b.strip())   # Removes the whitspace from the beginning and the end 
+
+c = "Hello World"
+print(c.replace("H","C"))  # Replace string with another string
+
+d = "Hello World"
+print(d.split(","))  # splits the string into substrings based on the separator
+
