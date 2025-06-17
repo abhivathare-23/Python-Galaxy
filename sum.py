85c54190e07dac196bb2de737c52e576490b95e4
Abhishek Vathare <107718018+abhivathare-23@users.noreply.github.com>
2025-06-17 18:38:29 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,8 @@
+# Sum of two numbers
+
+x = input("Enter first number : ")
+y = input("Enter second number : ")
+x = int(x)
+y = int(y)
+sum = x+y
+print("Sum of two number is ",sum)
---
+++ variable.py
@@ -0,0 +1,6 @@
+# Variable in python
+
+price = 100
+tax = 18
+total_price = price + tax
+print(total_price)
