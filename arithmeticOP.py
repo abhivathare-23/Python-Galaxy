e06f34a19e5519a3597edb80ec93708e7f9bc01d
Abhishek Vathare <107718018+abhivathare-23@users.noreply.github.com>
2025-06-18 16:12:07 +0530

    Add files via upload

---
+++ arithmeticOP.py
@@ -0,0 +1,44 @@
+#Addition Operator
+val1 = 4
+val2 = 5
+res = val1 + val2 
+print(res)
+
+#Subtraction Operator
+val3 = 3
+val4 = 2
+res = val3 - val4
+print(res)
+
+#Multiplication Operator 
+val5 = 42
+val6 = 10
+res = val5 * val6
+print(res)
+
+#Division Operator
+val7 = 12
+val8 = 3
+res = val7  / val8
+print(res)
+
+#Modulas Operator
+val9 = 20
+val10 = 3
+res = val9 % val10
+print(res)
+
+#Exponentiation Operator
+val11 = 3
+val12 = 3
+res = val11 ** val12
+print(res)
+
+#Floor Division operator
+val13 = 10
+val14 = 5
+res = val13 // val14
+print(res)
+
+
+
