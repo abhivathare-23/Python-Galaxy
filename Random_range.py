28e52af9c63f624e992314927f27c8c45cff1b2a
Abhishek Vathare <107718018+abhivathare-23@users.noreply.github.com>
2025-06-19 18:12:30 +0530

    Add files via upload

---
+++ Random_range.py
@@ -0,0 +1,11 @@
+# Random range using import random
+
+import random
+print(random.randrange(1,10))
+
+
+
+#belows code is for understand purpose using for loop
+
+for i in range(5):
+    print("Prints the range Between 1 and 10")
