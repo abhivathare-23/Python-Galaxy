0afa876c20e31559372999691ea6ac5f00f684d6
Abhishek Vathare <107718018+abhivathare-23@users.noreply.github.com>
2025-06-19 18:13:18 +0530

    Add files via upload

---
+++ Placeholders_and_modifiers.py
@@ -0,0 +1,23 @@
+# Placeholders and modifiers
+
+price = 50
+
+txt = f"The price is {price} dollars"
+print(txt)
+
+# placeholder can include modifiers to format the value
+
+
+# display calue with 2 decimals
+
+price1 = 40
+txt1 = f"The price is {price1:.2f} dollars"
+print(txt1)
+
+
+#Perform a math operation in the placeholder
+
+price2 = 20
+txt2 = f"The price is {20*40} dollars"
+print(txt2)
+
