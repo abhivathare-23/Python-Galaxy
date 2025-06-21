d975fa7541d7e048a5fc4b125209e998d06ba07b
Abhishek Vathare <107718018+abhivathare-23@users.noreply.github.com>
2025-06-21 20:11:16 +0530

    Add files via upload

---
+++ check_str.py
@@ -0,0 +1,22 @@
+#check string
+txt = "The best thing in life is free"
+print("thing" in txt)
+
+print("the" in txt)
+
+#it return true if the string is present in the text otherwise it returns false
+
+
+
+# print using if statment
+if "free" in txt:
+    print("free is present in text")
+
+
+# check if string is not present in text
+if "expensive" not in txt:
+    print("expensive" not in txt)
+
+if "expensive" not in txt:
+    print("expensive is not present in text")
+
