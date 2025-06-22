a8a0d52ec97aac9859bad4441a09e6f5be3ca24f
Abhishek Vathare <107718018+abhivathare-23@users.noreply.github.com>
2025-06-22 15:37:02 +0530

    Add files via upload

---
+++ boolean_values.py
@@ -0,0 +1,16 @@
+# Boolean values
+
+print(10>9)
+print(10==9)
+print(10<9)
+
+# Using if statement
+
+
+a = 200
+b = 34
+if b > a:
+    print("b is greater than 0")
+else:
+    print("b is not greater than 0")
+
