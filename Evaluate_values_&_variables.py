d7e67b22543b7074fd9ea89d52a7a75394f04456
Abhishek Vathare <107718018+abhivathare-23@users.noreply.github.com>
2025-06-22 15:39:17 +0530

    Add files via upload

---
+++ Evaluate_values_&_variables.py
@@ -0,0 +1,15 @@
+print(bool("hello"))
+print(bool(15))
+
+# Evaluates two variables
+x = "Hello"
+y = 10
+print(bool(x))
+print(bool(y))
+
+
+# Python also has many built-in functions that return a boolean value, 
+# like the isinstance() function, which can be used to determine if an object is of a certain data type:
+
+m = 200
+print(isinstance(m, int))
