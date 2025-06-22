9724d3485e3e63b73251d0c3120b026ac25ac47b
Abhishek Vathare <107718018+abhivathare-23@users.noreply.github.com>
2025-06-22 15:42:52 +0530

    Add files via upload

---
+++ if_elif stmt.py
@@ -0,0 +1,12 @@
+# shortcut of if-elif chain, while using if-elif statement at the end else block is added which
+# is performed if none of the above if-elif statment is true
+
+letter = "A"
+if letter == "B":
+    print("letter is B")
+elif letter == "C":
+    print("letter is C")
+elif letter == "A":
+    print("letter is A")
+else:
+    print("letter isn't A,B or C")
