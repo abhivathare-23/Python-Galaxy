f48869dcc71f76b0e1120d6601f986e35ee80b52
Abhishek Vathare <107718018+abhivathare-23@users.noreply.github.com>
2025-06-23 22:39:30 +0530

    Add files via upload

---
+++ chain_ifelse.py
@@ -0,0 +1,13 @@
+        # we can also chain if-else statement with more than one condition
+letter = "B"
+if letter == "B":
+    print("letter is B")
+else:
+    if letter == "C":
+     print("letter is C")
+    else:
+     if letter =="A":
+        print("letter is A")
+     else:
+           print("letter isn't A,B and C")
+
