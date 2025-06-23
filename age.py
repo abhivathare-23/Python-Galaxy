807d5ae24bf2c72b31567e7845f435ccb638aa20
Abhishek Vathare <107718018+abhivathare-23@users.noreply.github.com>
2025-06-23 22:42:33 +0530

    Add files via upload

---
+++ age.py
@@ -0,0 +1,9 @@
+# Check user is eligible for voting
+
+age=int(input("Enter your age : "))
+
+if age>=18:
+    print("You are eligible for vote!")
+else:
+    print("You are not eligible for vote!")
+    
