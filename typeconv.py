4e6a930ca6fb14406d91ef18a2ff36bca26fc694
Abhishek Vathare <107718018+abhivathare-23@users.noreply.github.com>
2025-06-25 15:25:53 +0530

    Add files via upload

---
+++ typeconv.py
@@ -0,0 +1,25 @@
+        # type conversion in python 
+
+#implicit
+a=10
+b=1.5
+c=a+b
+print(c)
+
+#Explicit
+s="135"
+i=1+int(s)
+f=float(s)
+print(i)
+print(f)
+
+j=20
+print(bin(j))
+print(hex(j))
+print(oct(j))
+
+d="geeks"
+print(list(d))
+print(tuple(d))
+print(set(d))
+
