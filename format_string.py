38b05ca4d8f339bce4ec9995606b7ba150951872
Abhishek Vathare <107718018+abhivathare-23@users.noreply.github.com>
2025-06-26 22:12:24 +0530

    Add files via upload

---
+++ format_string.py
@@ -0,0 +1,16 @@
+# format string
+
+
+# this wiil give error cause age is an integer
+
+"""
+age = 22
+txt = "My name is Abhi, I am " + age
+print(txt)   
+
+"""
+
+# create an f string
+age = 22
+txt = f"My name is Abhi, i am{age}"
+print(txt) 
