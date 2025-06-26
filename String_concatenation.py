e248bed62b22f489f326bbc73e94f52e79e4aaf8
Abhishek Vathare <107718018+abhivathare-23@users.noreply.github.com>
2025-06-26 22:11:08 +0530

    Add files via upload

---
+++ String_concatenation.py
@@ -0,0 +1,17 @@
+# String concatenation
+# combine two stringa we can use the + operator
+
+#Merge variable a with variable b into variable c:
+
+a = "Hello"
+b = "World"
+c = a+b
+print(c)
+
+
+#To add a space between them, add a " ":
+
+m = "Hello"
+n = "Java"
+x = m+ "" +n
+print(x)
