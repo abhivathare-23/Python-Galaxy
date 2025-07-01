e0a6bce3ffebabc8d8e53a94bbeab97ff9d1af4e
Abhishek Vathare <107718018+abhivathare-23@users.noreply.github.com>
2025-07-01 10:10:01 +0530

    Add files via upload

---
+++ add_iterable.py
@@ -0,0 +1,9 @@
+# add any iterable to the list using extend() method
+
+list = ["apple","banana","cherry"]
+tuple = ("kiwi","orange")
+
+list.extend(tuple)  # add items of tuple to the end of the list
+print(list)
+
+
