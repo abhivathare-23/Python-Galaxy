69384a6116688a5ae142c51756dad383852c40f2
Abhishek Vathare <107718018+abhivathare-23@users.noreply.github.com>
2025-07-01 10:09:31 +0530

    Add files via upload

---
+++ add_list_items.py
@@ -0,0 +1,13 @@
+# Add list items 
+# Add item to the end of the list use append() method
+
+fruits = ["apple","banana","cherry"]
+print(fruits)
+
+fruits.append("orange")  # add item to the end of the list
+print(fruits)
+
+fruits.insert(1,"kiwi")  # insert item at index 1
+print(fruits)
+
+
