08cbeefab7db2188ba06f4227e0d7859c57a846b
Abhishek Vathare <107718018+abhivathare-23@users.noreply.github.com>
2025-07-03 10:48:36 +0530

    Add files via upload

---
+++ Change_list_item.py
@@ -0,0 +1,26 @@
+# Change list items
+
+list = ["apple","Mango","Banana","Orange","Kiwi"]
+print(list)
+
+list[0] = "BMW" # change item at index 0
+print(list)
+
+print()
+
+list1 = ["abhi","sagar","rohit","sumit","kiran"]
+print(list1)
+
+list1[1:3] = ["AAA","BBB"]  # change items at index 1 and 2
+print(list1)
+
+
+print()
+#Change the second value by replacing it with two new values:
+
+list2 = ["apple","banana","cherry"]
+list2[1:2] = ["blackcurrant","watermelon"] # change item at index 1 and replace it with two new items
+print(list2)
+
+
+
