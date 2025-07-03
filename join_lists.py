a6077f1cccd223c47128056affd7a6552a2cd350
Abhishek Vathare <107718018+abhivathare-23@users.noreply.github.com>
2025-07-03 10:50:32 +0530

    Add files via upload

---
+++ join_lists.py
@@ -0,0 +1,27 @@
+# join lists or concatinate two or more lists using + operator
+
+list1 = ["a","b","c"]
+list2 = [1,2,3]
+list3 = list1 + list2 
+print(list3)
+
+
+print()
+# append list2 into list1
+car = ["bmw","audi","ford"]
+num = [1,2,3]
+for x in num:
+    car.append(x)
+print(car)
+
+
+
+print()
+# using extend() method to add elements from one list to another list
+fruits = ["mango","orange","banana"]
+vegies = ["flower","tomato","onion"]
+fruits.extend(vegies)
+print(fruits)
+
+
+
