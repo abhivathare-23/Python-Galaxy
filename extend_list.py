55bedffb7765d071d5ded94abb1119acc2e0ac83
Abhishek Vathare <107718018+abhivathare-23@users.noreply.github.com>
2025-07-04 19:33:45 +0530

    Add files via upload

---
+++ extend_list.py
@@ -0,0 +1,12 @@
+# Extend list 
+# append elements of another list to the end of the current list, use extend() method
+
+list = ["apple","banana","cherry"]
+print(list)
+
+tropical = ["mango","pineapple","papaya"]
+print(tropical)
+
+list.extend(tropical)      # append elements of tropical to list at the end
+print(list)
+
