dbeec382ebdffb9414374b83a9de6395304129e0
Abhishek Vathare <107718018+abhivathare-23@users.noreply.github.com>
2025-07-04 19:33:24 +0530

    Add files via upload

---
+++ insert_items_list.py
@@ -0,0 +1,11 @@
+# Insert a new list item
+# The insert() method inserts an item at the specified index:
+
+list = ["apple","banana","cherry"]
+print(list)
+
+list.insert(2,"watermelon")     # insert item at index 2
+
+print(list)
+
+
