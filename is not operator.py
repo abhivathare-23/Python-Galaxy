484f3cab91fc61ba15ee441ceb607bfe1aa2bd53
Abhishek Vathare <107718018+abhivathare-23@users.noreply.github.com>
2025-07-05 19:14:40 +0530

    Add files via upload

---
+++ is not operator.py
@@ -0,0 +1,13 @@
+# is not operator
+x=5
+if(type(x) is not int):
+    print("true")
+else:
+    print("false")
+
+    # prints true
+    x=5.6
+    if(type(x)is not int):
+        print("true")
+    else:
+        print("false")
