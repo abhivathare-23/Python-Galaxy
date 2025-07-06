0750807ef7be8a63df64b4c413e732eea09ba2eb
Abhishek Vathare <107718018+abhivathare-23@users.noreply.github.com>
2025-07-06 22:32:21 +0530

    Add files via upload

---
+++ join_tuples.py
@@ -0,0 +1,17 @@
+# join tuples
+# join two or more tuples you can use + operator
+
+tuple1 = ("a","b","c")
+tuple2 = (1,2,3)
+
+tuple3 = tuple1 + tuple2 
+print(tuple3)
+
+
+
+print()
+# multiply tuple using *
+
+fruits = ("mango","banana","cherry")
+thistuple = fruits * 2 
+print(thistuple)
