11390030a0df203f2c1633d008081f45268942fd
Abhishek Vathare <107718018+abhivathare-23@users.noreply.github.com>
2025-07-06 22:32:01 +0530

    Add files via upload

---
+++ learn_tuples.py
@@ -0,0 +1,29 @@
+# learn tuples
+
+tuple = ("apple","banana","mango")
+print(tuple)
+
+
+#tuple items are ordered, unchangable and allow duplicate values 
+#tuple items are indexed - the first item has index [0]
+
+# ordered - tuples are ordered, it means items have a defined order, and that order will not change
+# unchangable - means we cannot change, add or remove items after tuple has been created
+# allow duplicates - tuples are indexed, they can have items with same value
+print()
+
+cars = ("bmw","mercedes","ford","mustang","bmw")
+print(cars)
+
+print(len(cars))
+
+print()
+
+vegies = ("flower",)  
+print(type(vegies))  
+#To create a tuple with only one item, you have to add a comma after the item, 
+# otherwise Python will not recognize it as a tuple.
+
+
+
+
