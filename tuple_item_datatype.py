8a192b5fadf3ae96ad7083676a16b1bc4968dc11
Abhishek Vathare <107718018+abhivathare-23@users.noreply.github.com>
2025-07-08 19:04:56 +0530

    Add files via upload

---
+++ tuple_item_datatype.py
@@ -0,0 +1,26 @@
+# tuple item datatype
+
+tuple1 = ("apple","banana","cherry")
+tuple2 = (1,2,4,8,3)
+tuple3 = (True, False, False)
+
+print(tuple1)
+print(tuple2)
+print(tuple3)
+
+print()
+# tuple can contain different datatypes
+thistuple = ("abc",2,True,30,False)
+print(thistuple)
+
+print()
+# type()
+mytuple = ("apple","banana","cherry")
+print(type(mytuple))
+
+print()
+# tuple constructor - also possible to make tuple() constructor to make tuple
+# using tuple() method to make tuple
+
+thistuple2 = tuple(("apple","banana","cherry"))
+print(thistuple2)
