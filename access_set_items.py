43946c5332fc58a904233f2df03e5ad37232110a
Abhishek Vathare <107718018+abhivathare-23@users.noreply.github.com>
2025-07-15 14:41:43 +0530

    Add files via upload

---
+++ access_set_items.py
@@ -0,0 +1,18 @@
+# access set items
+# we cannot access items in set by referring an indexm or key
+# using for loop we can do 
+
+set = {"mango","apple","banana"}
+for i in set:
+    print(i)
+    
+
+print()
+# check if banana is present in the set
+print("banana" in set)
+
+print()
+# check if banana is not present in the set
+print("banana" not in set)
+
+# once set created we cannot change its items, but we can add new items
